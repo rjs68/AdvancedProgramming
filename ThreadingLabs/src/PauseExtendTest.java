60c54f0989c4456dd0e9bc64a4164e5a19a7462c
Rhys <rhys-stephens@hotmail.co.uk>
2020-02-26 14:07:47 +0000

    Created multiple threads and named them to track actions

--- ThreadingLabs/src/PauseExtendTest.java
+++ ThreadingLabs/src/PauseExtendTest.java
@@ -3,26 +3,31 @@ import java.util.Random;
 public class PauseExtendTest {
     private static class PauseExtend extends Thread{
         private int pause;
+        private String name;
 
-        public PauseExtend(){
+        public PauseExtend(int i){
+            this.name = "Thread " + i;
             Random rand = new Random();
             pause = rand.nextInt(5);
         }
 
         public void run(){
-            System.out.println("Pausing for " + pause + " seconds");
+            System.out.println(name + " is pausing for " + pause + " seconds");
             try {
                 Thread.sleep(pause*1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }finally {
-                System.out.println("Now we're ready to go boiiiii");
+                System.out.println(name + " is ready to go!");
             }
         }
     }
 
     public static void main(String args[]){
-        PauseExtend pause = new PauseExtend();
-        pause.start();
+        PauseExtend[] pauses = new PauseExtend[3];
+        for(int i=0; i<pauses.length; i++){
+            pauses[i] = new PauseExtend(i+1);
+            pauses[i].start();
+        }
     }
 }
